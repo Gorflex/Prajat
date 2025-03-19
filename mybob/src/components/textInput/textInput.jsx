c46cc11f8494938e2b093a21b8ef2c2033d4f608
Ion B <Galdurbox@gmail.com>
2025-03-20 01:14:13 +0200

    Abobatar

---
+++ mybob/src/components/textInput/textInput.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./textInput.css";
+
+function TextInput() {
+  return (
+    <div className="container1">
+      <div className="main1">
+        <h1>Text: <input name="myInput" /></h1>
+      </div>
+    </div>
+  );
+}
+
+export default TextInput;
--- mybob/src/components/topNavigation/topNavigation.jsx
+++ mybob/src/components/topNavigation/topNavigation.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./topNavigation.css";
 
-function Penis() {
+function TopBanner() {
   return (
     <div className="container">
       <div className="main">
-        <h1>PENIS</h1>
+        <h1>pizdets</h1>
       </div>
     </div>
   );
 }
 
-export default Penis;
+export default TopBanner;
